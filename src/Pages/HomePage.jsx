a9e1c018b5c2f72e0eda658348a0fa31c0611a07
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-09-18 23:05:36 +0530

    Homw Section Remove

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import HomeCarousel from "../Components/Carosel/HomeCarousel";
-import AllCategories from "../Components/Home/AllCategories";
 
 const HomePage = () => {
   return (
     <div>
       <HomeCarousel />
-      <AllCategories />
     </div>
   );
 };
